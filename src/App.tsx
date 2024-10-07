6d1897bb6958298df11fbf96e8e4294f5b577df4
Ivan Ivanov <aydikstudio@mail.ru>
2024-10-07 09:15:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import { Table, Button, Checkbox, Form, Input, Space } from 'antd';
+import { useSelector } from 'react-redux';
+import { New } from './interface';
+import { useDispatch } from 'react-redux';
+import { addNewNews, deleteNews, editNews } from './redux/features/news/newsSlice';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [status, setStatus] = useState<string>('create');
+    const [timeObj, setTimeObj] = useState<New>({
+        title: '',
+        description: ''
+    });
+
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+    const data: any = useSelector<New[]>((state:any) => state.news.value);
+    const dispatch = useDispatch();
+
+    
+
+
+    const columns = [
+        {
+            title: 'Title',
+            dataIndex: 'title',
+            key: 'title',
+            render: (text: any) => <a>{text}</a>,
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_: any, record: any) => (
+                <Space size="middle">
+                  
+                    <a  onClick={() => startEdit(record)}>Редактировать</a>
+                    <a onClick={() => dispatch(deleteNews(record))}>Удалить</a>
+                </Space>
+            ),
+        },
+    ];
+
+
+    const onAdd = () => {
+        if(title.length > 0 && description.length > 0) {
+            dispatch(addNewNews({title, description}))
+            setTitle('')
+            setDescription('')
+        } else {
+            alert("Заполните все поля")
+        }
+        }
+
+    const cancelEdit = () => {
+        setTitle('')
+        setDescription('')
+        setTimeObj({
+            title: '',
+            description: ''
+        });
+    }
+
+    const startEdit = (item: New) => {
+        setTimeObj(item);
+        setStatus('edit');
+        setTitle(item.title);
+        setDescription(item.description);
+    }
+
+    const onEdit =() => {
+        dispatch(editNews({
+            oldTitle: timeObj.title,
+            title,
+            description
+        }))
+
+        cancelEdit()
+    }
+  
+
+    return (
+
+        <div style={{
+            display: 'block', width: 700, padding: 30, margin: '0 auto'
+        }}>
+
+            <Form>
+                <Form.Item label="Добавить новость" name="layout">
+                </Form.Item>
+                <Form.Item>
+                    <Input placeholder="Наименование" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </Form.Item>
+                <Form.Item>
+                    <Input placeholder="Описание" value={description} onChange={(e) => setDescription(e.target.value)} />
+                </Form.Item>
+                <Form.Item>
+                {status == 'edit' ?   <Button type="primary" onClick={onEdit}>Редактировать</Button>:   <Button type="primary" onClick={onAdd}>Добавить</Button>}
+                   {status == 'edit' && <a onClick={() => cancelEdit()}>Отменить редактирование</a>}
+                </Form.Item>
+            </Form>
+
+          <Table dataSource={data} columns={columns} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/redux/features/news/newsSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice  } from '@reduxjs/toolkit'
+
+
+
+export const newsSlice = createSlice({
+    name: 'news',
+    initialState: {
+        value: JSON.parse(localStorage.getItem('news') || '[]'),
+    },
+    reducers: {
+        addNewNews: (state:any, action) => {
+            state.value =  addValue(state, action);
+        },
+        editNews: (state: any, action) => {
+            state.value = editValue(state, action);
+        },
+        deleteNews: (state: any, action) => {
+            state.value = deleteValue(state, action);
+        },
+
+    },
+})
+
+export const { addNewNews, editNews,  deleteNews } = newsSlice.actions
+
+export default newsSlice.reducer
+
+function deleteValue(state:any, action:any) {
+    let new_array = [...state.value].filter((item) => item.title != action.payload.title);
+    localStorage.setItem('news', JSON.stringify(new_array));
+    return new_array;
+}
+
+
+function editValue(state:any, action:any) {
+    let obj = action.payload;
+    let new_array = [...state.value];
+    new_array.map((item) => {
+        if(item.title == obj.oldTitle) {
+            item.title = obj.title;
+            item.description = obj.description;
+        }
+        return item;
+    });
+    localStorage.setItem('news', JSON.stringify(new_array));
+    return new_array;
+}
+
+
+function addValue(state:any, action:any) {
+    let new_array = [...state.value];
+    Object.freeze(action.payload);
+    new_array.push(action.payload);
+    localStorage.setItem('news', JSON.stringify(new_array));
+    return new_array;
+}
